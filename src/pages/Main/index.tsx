b785a360d2ca8b0c99976861ccd76135b6a1b29b
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-08-28 15:31:52 +0900

    [feat] 메인페이지

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,14 +1,58 @@
-import React from 'react';
-import Header from './Header';
+import React, { useState } from 'react';
 import Banner from './Banner';
+import { styled } from 'styled-components';
 
 const Main = () => {
+  const [common, setCommon] = useState(true);
+  const onClickMenu = (booleanValue: boolean) => {
+    setCommon(booleanValue);
+  };
+
   return (
     <div>
-      <Header />
       <Banner />
+      <SelectMenu>
+        <h3
+          onClick={() => onClickMenu(true)}
+          style={common ? { fontWeight: 700 } : { fontWeight: 500 }}
+        >
+          경매
+        </h3>
+        <div />
+        <h3
+          onClick={() => onClickMenu(false)}
+          style={common ? { fontWeight: 500 } : { fontWeight: 700 }}
+        >
+          일반
+        </h3>
+      </SelectMenu>
+      <div
+        style={{ width: '100%', height: '1px', backgroundColor: '#333333' }}
+      />
     </div>
   );
 };
+
+const SelectMenu = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: row;
+  padding: 20px 120px;
+  box-sizing: border-box;
+  h3 {
+    font-size: 20px;
+    margin: 0 20px 1px 0;
+    font-weight: 500;
+    cursor: pointer;
+  }
+  div {
+    width: 1px;
+    height: 24px;
+    background-color: #333333;
+    margin-right: 20px;
+  }
+`;
 
 export default Main;
