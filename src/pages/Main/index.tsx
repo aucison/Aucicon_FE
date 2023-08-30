77374267d739edd742016ca03e4eed6222128862
MinJaeSon <mjsonsj@gmail.com>
2023-08-30 18:55:32 +0900

    [del] 헤더 삭제

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from './Header';
 import Banner from './Banner';
 import Items from './Items';
 
 const Main = () => {
   return (
     <div>
-      <Header />
       <Banner />
       <Items title="Hot Item" subtitle="인기 경매 상품" />
       <Items title="New Item" subtitle="새 경매 상품" />
