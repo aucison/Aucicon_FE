2d814baa5df66bc622f3f926a4517bd5fa8806b2
MinJaeSon <mjsonsj@gmail.com>
2023-08-30 18:57:03 +0900

    [add] 좋아요 페이지 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main';
+// import Main from './pages/Main';
 import Sell from './pages/Sell';
 import { createGlobalStyle } from 'styled-components';
 import Header from './pages/Main/Header';
+import ItemList from './pages/ItemList';
+import LikeList from './pages/Like/LikeList';
 
 function App() {
   return (
@@ -13,9 +15,10 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Main />} />
+          <Route path="/" element={<ItemList />} />
           <Route path="/mypage" element={<div>My Page</div>} />
           <Route path="/sell" element={<Sell />} />
+          <Route path="/like" element={<LikeList />} />
           <Route path="*" element={<div>Not Found</div>} />
         </Routes>
       </Router>
