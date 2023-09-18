6c30ad05d955111c5ae317dbce089c1930d53f1a
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-09-18 23:05:58 +0900

    [feat] 미리보기 스타일 변경

--- src/pages/Sell/Preview.tsx
+++ src/pages/Sell/Preview.tsx
@@ -8,18 +8,35 @@ interface PreviewProps {
 const Preview = (props: PreviewProps) => {
   const { imageSrc } = props;
   return (
-    imageSrc &&
-    imageSrc.map((src, index) => (
-      <Thumbnail key={index}>
-        <img width="100%" src={src} />
-      </Thumbnail>
-    ))
+    <Container>
+      {imageSrc &&
+        imageSrc.map((src, index) => (
+          <Thumbnail key={index}>
+            <img width="100%" src={src} />
+          </Thumbnail>
+        ))}
+    </Container>
   );
 };
 
 const Thumbnail = styled.div`
   width: 100px;
   height: 100px;
+  border-radius: 5px;
+  img {
+    width: 100px;
+    height: 100px;
+    border-radius: 5px;
+  }
+  & + & {
+    margin-left: 24px;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  margin-left: 16px;
+  margin-top: 40px !important;
 `;
 
 export default Preview;
